d1db677e8003ad464168835e8bf03b3800d96510
Emiliano Monson <emimonson8gmail.com>
2026-02-09 20:49:36 -0300

    Actualizacion de diseñÃos y formularios

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,13 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static', // Optimiza para que todo sea HTML puro y rápido
+  compressHTML: true, // Reduce el tamaño del archivo final
+  build: {
+    inlineStylesheets: 'always', // Evita el "parpadeo" de estilos al cargar
+  },
+  prefetch: {
+    prefetchAll: true, // Descarga las secciones antes de que el usuario haga clic
+    defaultStrategy: 'viewport',
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'], // Asegúrate de que esta línea esté igual
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
   theme: {
     extend: {
       colors: {
-        'nja-dark': '#0a0a0a',
         'nja-green': '#00FF41',
         'nja-cyan': '#00f2fe',
       },
+      // Unificamos la fuente Inter que definimos en el Layout
       fontFamily: {
-        'tech': ['Orbitron', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+      backgroundImage: {
+        'gradient-nja': 'linear-gradient(to right, #00FF41, #00f2fe, #ffffff)',
       },
     },
   },
